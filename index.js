187d65783623764ba335231027b5e9b4e8c4cda0
Florent Kolosowski <fkolosowski@ivsweb.com>
2018-03-05 10:35:19 +0100

    uclass to es7 class / use eslint plugin

--- index.js
+++ index.js
@@ -1,107 +1,100 @@
 "use strict";
 
-var Class    = require('uclass');
-var onRemove = require('udom/element/onRemove');
+const deepMixIn = require('mout/object/deepMixIn');
+const onRemove  = require('udom/element/onRemove');
 
-var csSpriteLite = new Class({
-  Binds : ['stop'],
+class csSpriteLite {
 
-  Implements : [
-    require('uclass/options')
-  ],
+  constructor(options) {
+    this.stop = this.stop.bind(this);
 
-  options : {
-    url : null,
-    width : 200,
-    height : 200,
-    interval : 100,
-    anchor : document.body,
-    loop_from : 0,
-    ignored_frames : [],
-    loop_start_coor : {
-      x : 0, y : 0
-    },
-    current_position : {
-      x : 0, y : 0
-    }
-  },
+    this.options = {
+      url       : null,
+      width     : 200,
+      height    : 200,
+      interval  : 100,
+      anchor    : document.body,
+      loop_from : 0,
+      ignored_frames   : [],
+      loop_start_coor  : {
+        x : 0, y : 0
+      },
+      current_position : {
+        x : 0, y : 0
+      }
+    };
 
-  interval : null,
+    this.interval = null;
 
-  initialize : function(options) {
-    var self = this;
-    self.setOptions(options);
-  },
+    deepMixIn(this.options, (options || {}));
+  }
 
-  nextStep : function() {
-    var self = this;
-
-    if (self.options.loop_from == self.frame && !self.loop_flag) {
-      self.options.loop_start_coor = {x : self.options.current_position.x, y : self.options.current_position.y};
-      self.loop_flag = true;
+  nextStep() {
+    if(this.options.loop_from == this.frame && !this.loop_flag) {
+      this.options.loop_start_coor = {x : this.options.current_position.x, y : this.options.current_position.y};
+      this.loop_flag = true;
     }
 
-    self.options.current_position.x += self.options.width;
-    if (self.img_size.width <= self.options.current_position.x) 
-      self.options.current_position = {x : 0, y : self.options.current_position.y + self.options.height};
+    this.options.current_position.x += this.options.width;
+    if(this.img_size.width <= this.options.current_position.x)
+      this.options.current_position = {x : 0, y : this.options.current_position.y + this.options.height};
 
-    if (self.img_size.height <= self.options.current_position.y) {
-      self.frame = self.options.loop_from;
-      self.options.current_position = {x : self.options.loop_start_coor.x, y : self.options.loop_start_coor.y};
+    if(this.img_size.height <= this.options.current_position.y) {
+      this.frame = this.options.loop_from;
+      this.options.current_position = {x : this.options.loop_start_coor.x, y : this.options.loop_start_coor.y};
     }
 
-    if (self.options.ignored_frames.indexOf(self.frame) !== -1)
-      self.nextStep();
-  },
+    if(this.options.ignored_frames.indexOf(this.frame) !== -1)
+      this.nextStep();
+  }
 
-  setStyles : function(obj, propertyObject) {
-   for (var property in propertyObject)
+  setStyles(obj, propertyObject) {
+    for(var property in propertyObject)
       obj.style[property] = propertyObject[property];
-  },
+  }
 
-  play : function() {
-    var self = this,
-        src_img = new Image();
+  play() {
+    var src_img = new Image();
 
-    src_img.onload = function() {
-      self.img_size = {
-        'width' : this.width,
-        'height' : this.height
+    src_img.onload = () => {
+      this.img_size = {
+        width  : this.width,
+        height : this.height
       };
 
       var canvas = document.createElement("div");
       canvas.id  = "css-sprite";
 
-      self.setStyles(canvas, {
-        "backgroundImage"    : "url('" + self.options.url + "')",
-        "backgroundRepeat"   : "no-repeat",
-        "backgroundPosition" : "0px 0px",
-        "width"              : self.options.width || 200,
-        "height"             : self.options.height || 200
+      this.setStyles(canvas, {
+        backgroundImage    : `url('${this.options.url}')`,
+        backgroundRepeat   : "no-repeat",
+        backgroundPosition : "0px 0px",
+        width              : this.options.width || 200,
+        height             : this.options.height || 200
       });
 
-      self.options.anchor.appendChild(canvas);
-      onRemove(canvas, self.stop);
-      self.frame = 0,
-      self.loop_flag = false;
+      this.options.anchor.appendChild(canvas);
+      onRemove(canvas, this.stop);
+      this.frame = 0,
+      this.loop_flag = false;
 
-      self.interval = setInterval(function() {
-        self.nextStep();
-        self.setStyles(canvas, {
-          "backgroundPosition" : "-" + self.options.current_position.x + "px -" + self.options.current_position.y + "px"
+      this.interval = setInterval(() => {
+        this.nextStep();
+        this.setStyles(canvas, {
+          "backgroundPosition" : `-${this.options.current_position.x}px -${this.options.current_position.y}px`
         });
 
-        self.frame++;
-      }, self.options.interval);
+        this.frame++;
+      }, this.options.interval);
     };
-    src_img.src = self.options.url;
-  },
 
-  stop : function() {
-    var self = this;
-    clearInterval(self.interval);
+    src_img.src = this.options.url;
   }
 
-});
+  stop() {
+    clearInterval(this.interval);
+  }
+
+}
 
 module.exports = csSpriteLite;
