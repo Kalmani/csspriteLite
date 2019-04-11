ef80e518fc1fb461cc5c75a2ad4993fceb0f3b89
Florent Kolosowski <fkolosowski@ivsweb.com>
2019-04-11 12:45:32 +0200

    add goTo / pauseTo methodes, expose onLoaded callback

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 csSpriteLite is a standalone and light script to animate sprites.
 
-### Tech
-Pure javascript, no dependencies to any libraries !
-
 ### Installation
 ```
 npm install css-sprite-lite
@@ -12,19 +9,24 @@ npm install css-sprite-lite
 
 ### Example
 ```
-const csSpriteLite = require('csSpriteLite');
+const csSpriteLite = require('css-sprite-lite');
  
 let sprite = new csSpriteLite({
     anchor : document.getElementById('cssprit-container'),
     url : "sprite.png",
+    classname : 'class_1 class_2'
     width  : 184,
     height : 325,
     loop_from : 8,
     ignored_frames : [29, 30],
-    interval : 100
+    interval : 100,
+    onLoaded : function(sprite) {
+      sprite.goTo(5);
+      sprite.play();
+      sprite.pauseTo(60);
+    }
 });
 
-sprite.play();
 
 <div id="cssprit-container"></div>
 ```
@@ -32,10 +34,12 @@ sprite.play();
 ### Options
 * anchor : The dom element you want to display your sprite into. Default is document.body
 * url : The file path of your sprite
+* classname : Add passed class to the sprite container
 * width / height : The size of a UNIQUE element in your sprite
 * interval : Time (in ms) between to elements of your sprite. Default is 100
 * loop_from : Just in case you don't want to loop from the beginning of your sprite (imagine an intro for exemple). Default is 0
 * ignored_frames : Array of elements you eventually want to ignore.
+* onLoaded : Callback used when the source sprite is loaded.
 
 ### NB
 loop_from and ignored_frames options work with frame numbers : for exemple, if you have 8 frames of intro, you should loop from frame 8 (begin with frame 0, same thing for ignored_frames)...
@@ -49,13 +53,17 @@ play()
 pause()
   Pause animation on current frame.
 
+goTo(frame_key)
+  Alias of pause method, point to passed frame key.
+
+pauseTo(frame_key)
+  Will play untill frame key.
+
 ```
 
 ### Todos
  - Accept coords for ignored_frames and loop_from
  - Write tests, tests, TESTS !
- - Add pause / restart methods
 
 ### License
 MIT License style, please distribute & credit me somewhere.
- 
--- index.js
+++ index.js
@@ -7,43 +7,88 @@ class csSpriteLite {
 
   constructor(options) {
     this.pause   = this.pause.bind(this);
+    this.goTo    = this.pause.bind(this);
+    this.play    = this.createInterval.bind(this);
+
+    this.pauseTo = (frame) => this.pause_to = frame;
+
     this.options = {
       url       : null,
       width     : 200,
       height    : 200,
       interval  : 100,
+      classname : '',
       anchor    : document.body,
       loop_from : 0,
-      ignored_frames   : [],
-      loop_start_coor  : {
-        x : 0, y : 0
-      },
-      current_position : {
-        x : 0, y : 0
-      }
+      onLoaded  : Function.prototype,
+      ignored_frames   : []
     };
 
-    self.canvas   = null;
+    this.loop_start_coor  = {x : 0, y : 0};
+    this.current_position = {x : 0, y : 0};
+
+    this.pause_to = null;
+    this.frame    = null;
+    this.canvas   = null;
     this.img_size = null;
     this.interval = null;
 
     deepMixIn(this.options, (options || {}));
+
+    this.loadSrc();
+  }
+
+  loadSrc() {
+    var src_img = new Image();
+
+    src_img.onload = (evt) => {
+      let src = evt.target;
+
+      this.img_size = {
+        width  : src.width,
+        height : src.height
+      };
+
+      this.canvas = document.createElement('div');
+
+      this.canvas.classList.add('css-sprite');
+      this.options.classname.split(' ').forEach((classname) => (classname) ? this.canvas.classList.add(classname) : null);
+
+      this.setStyles(this.canvas, {
+        backgroundImage    : `url('${this.options.url}')`,
+        backgroundRepeat   : 'no-repeat',
+        backgroundPosition : '0px 0px',
+        width              : this.options.width || 200,
+        height             : this.options.height || 200
+      });
+
+      this.options.anchor.appendChild(this.canvas);
+
+      onRemove(this.canvas, this.pause);
+
+      this.frame     = 0;
+      this.loop_flag = false;
+
+      this.options.onLoaded(this);
+    };
+
+    src_img.src = this.options.url;
   }
 
   nextStep() {
     if(this.options.loop_from == this.frame && !this.loop_flag) {
-      this.options.loop_start_coor = {x : this.options.current_position.x, y : this.options.current_position.y};
+      this.loop_start_coor = {x : this.current_position.x, y : this.current_position.y};
       this.loop_flag = true;
     }
 
-    this.options.current_position.x += this.options.width;
+    this.current_position.x += this.options.width;
 
-    if(this.img_size.width <= this.options.current_position.x)
-      this.options.current_position = {x : 0, y : this.options.current_position.y + this.options.height};
+    if(this.img_size.width <= this.current_position.x)
+      this.current_position = {x : 0, y : this.current_position.y + this.options.height};
 
-    if(this.img_size.height <= this.options.current_position.y) {
+    if(this.img_size.height <= this.current_position.y) {
       this.frame = this.options.loop_from;
-      this.options.current_position = {x : this.options.loop_start_coor.x, y : this.options.loop_start_coor.y};
+      this.current_position = {x : this.loop_start_coor.x, y : this.loop_start_coor.y};
     }
 
     if(this.options.ignored_frames.indexOf(this.frame) !== -1)
@@ -55,56 +100,42 @@ class csSpriteLite {
       obj.style[property] = propertyObject[property];
   }
 
-  play() {
-    if(this.canvas)
-      return this.createInterval();
-
-    var src_img = new Image();
-    var self    = this;
-
-    src_img.onload = function() {
-      self.img_size = {
-        width  : this.width,
-        height : this.height
-      };
-
-      self.canvas     = document.createElement('div');
-      self.canvas.id  = 'css-sprite';
-
-      self.setStyles(self.canvas, {
-        backgroundImage    : `url('${self.options.url}')`,
-        backgroundRepeat   : 'no-repeat',
-        backgroundPosition : '0px 0px',
-        width              : self.options.width || 200,
-        height             : self.options.height || 200
-      });
-
-      self.options.anchor.appendChild(self.canvas);
-
-      onRemove(self.canvas, self.pause);
-
-      self.frame     = 0;
-      self.loop_flag = false;
-
-      self.createInterval();
-    };
-
-    src_img.src = this.options.url;
-  }
-
   createInterval() {
     this.interval = setInterval(() => {
+      if(this.pause_to && this.frame == this.pause_to)
+        return this.pause();
+
       this.nextStep();
       this.setStyles(this.canvas, {
-        backgroundPosition : `-${this.options.current_position.x}px -${this.options.current_position.y}px`
+        backgroundPosition : `-${this.current_position.x}px -${this.current_position.y}px`
       });
 
       this.frame++;
     }, this.options.interval);
   }
 
-  pause() {
+  pause(frame) {
     clearInterval(this.interval);
+
+    this.pause_to = null;
+
+    if(frame !== undefined) {
+      this.frame            = 0;
+      this.current_position = {x : 0, y : 0};
+
+      for(let i = 0; i <= frame; i++) {
+        this.current_position.x += this.options.width;
+
+        if(this.img_size.width <= this.current_position.x)
+          this.current_position = {x : 0, y : this.current_position.y + this.options.height};
+
+        this.frame++;
+      }
+
+      this.setStyles(this.canvas, {
+        backgroundPosition : `-${this.current_position.x}px -${this.current_position.y}px`
+      });
+    }
   }
 
 }
